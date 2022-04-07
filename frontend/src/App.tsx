556ce0c800d85b1d38afa260204ee931b715bc2a
Eduardo Pina <jeseduardopi@gmail.com>
2022-04-07 23:58:13 +0200

    Add backend test to db Add form to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,31 +1,34 @@
 import { useEffect } from 'react'
+import { InscriptionForm } from './components/InscriptionForm'
 
 
 function App() {
-  
-    useEffect(() => {  
-    const body = new URLSearchParams({
+
+    useEffect(() => {
+        const body = new URLSearchParams({
             name: 'Eduardo',
             lastname: 'Pina'
-    })
+        })
 
-    const header = new Headers({
-        'Content-type':'application/x-www-form-urlencoded'
-    })
+        const header = new Headers({
+            'Content-type': 'application/x-www-form-urlencoded'
+        })
         fetch('http://localhost:3456', {
             method: 'POST',
             body: body,
             headers: header,
             mode: 'cors'
         })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        
+            .then(res => res.json())
+            .then(data => console.log(data))
+
     }, [])
 
-  return (
-    <></>
-  )
+    return (
+        <>
+            <InscriptionForm />
+        </>
+    )
 }
 
 export default App
